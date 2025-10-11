b11e4a2f676e51e5fe33abeae6c928912cfc2b79
userMauro-code <m88642336@gmail.com>
2025-10-11 14:58:34 -0300

    agregado de trancision

--- js/app.js
+++ js/app.js
@@ -1,20 +1,37 @@
 const buttonNav = document.getElementById("btnMenuNav");
 const menu = document.getElementById("menuNav");
-const imgMenu = document.querySelector("img");
+const imgMenu = document.querySelector(".imgMenu");
 const fondo = document.querySelector(".fondo");
 
 
 buttonNav.addEventListener("click", () => {
     
     if(buttonNav.getAttribute("aria-expanded") === "false"){
-        menu.hidden = false;
+        // menu.hidden = false;
+        menu.classList.add("menu__visible");
         buttonNav.setAttribute("aria-expanded", "true");
         imgMenu.setAttribute("src", "../images/icon-close.svg");
         fondo.classList.add("active");
+        fondo.classList.add("active__fondo")
+        document.body.style.overflow = "hidden";
+        
     }else{
-        menu.hidden = true;
+        // menu.hidden = true;
+        menu.classList.remove("menu__visible");
         buttonNav.setAttribute("aria-expanded", "false");
         imgMenu.setAttribute("src", "../images/icon-menu.svg");
         fondo.classList.remove("active");
+        fondo.classList.remove("active__fondo");
+        document.body.style.overflow= "auto";
+
     }
+});
+
+fondo.addEventListener("click", ()=> {
+    // menu.hidden = true;
+    menu.classList.remove("menu__visible");
+    buttonNav.setAttribute("aria-expanded", "false");
+    imgMenu.setAttribute("src", "../images/icon-menu.svg");
+    fondo.classList.remove("active");
+    fondo.classList.remove("active__fondo");
 });
