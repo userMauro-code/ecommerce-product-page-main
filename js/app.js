6afa635baf4d4205cc06169cab001590d437b2cf
userMauro-code <m88642336@gmail.com>
2025-10-12 15:21:44 -0300

    agregado de carrito

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
+// -------------------menu mobile-----------------
 const buttonNav = document.getElementById("btnMenuNav");
 const menu = document.getElementById("menuNav");
 const imgMenu = document.querySelector(".imgMenu");
 const fondo = document.querySelector(".fondo");
+// --------------cart mobile----------
+const buttonCart = document.getElementById("button__cart");
+const boxCart = document.getElementById("box__cart");
+const cartTitle = document.querySelector(".cart__title")
 
-
+// -------------------------menu mobile--------
 buttonNav.addEventListener("click", () => {
     
     if(buttonNav.getAttribute("aria-expanded") === "false"){
-        // menu.hidden = false;
         menu.classList.add("menu__visible");
         buttonNav.setAttribute("aria-expanded", "true");
         imgMenu.setAttribute("src", "../images/icon-close.svg");
@@ -16,7 +20,6 @@ buttonNav.addEventListener("click", () => {
         document.body.style.overflow = "hidden";
         
     }else{
-        // menu.hidden = true;
         menu.classList.remove("menu__visible");
         buttonNav.setAttribute("aria-expanded", "false");
         imgMenu.setAttribute("src", "../images/icon-menu.svg");
@@ -35,3 +38,20 @@ fondo.addEventListener("click", ()=> {
     fondo.classList.remove("active");
     fondo.classList.remove("active__fondo");
 });
+// -----------menu mobile finalizado--------------
+
+// -------------cart mobile-----------------
+buttonCart.addEventListener("click", ()=>{
+
+    if(buttonCart.getAttribute("aria-expanded")=== "false"){
+        boxCart.classList.add("cart__visible");
+        buttonCart.setAttribute("aria-expanded", "true");
+        cartTitle.hidden = false;
+    }else{
+        boxCart.classList.remove("cart__visible");
+        buttonCart.setAttribute("aria-expanded", "false");
+        cartTitle.hidden= true;
+       
+
+    }
+})
